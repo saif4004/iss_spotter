cf17493fbcff9c17c1f4ee37e8ec48a2044fbb3b
Saif Al-saedi <saif4004@gmail.com>
2024-09-26 15:45:39 -0700

    Implemented fetchCoordsByIP function and exported it

--- iss.js
+++ iss.js
@@ -13,18 +13,36 @@ const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   needle.get('https://api.ipify.org?format=json', (error,response,body) => {
     if (error) {
-      return callback(error),null;
+      return callback(error);
     }
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
       return;
     }
-   
     callback(null,body.ip);
     
   });
-
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip,callback) {
+  needle.get(`http://ipwho.is/${ip}`, (error,response,body) => {
+    if (error) {
+      callback(error,null);
+      return;
+    }
+  
+    if (!body.success) {
+      const message = `status was ${body.success}. Server said: ${body.message} when trying to get IP ${body.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    const latitude = body.latitude;
+    const longitude = body.longitude;
+    callback(null,{latitude, longitude});
+  });
+};
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP };
